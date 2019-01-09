d85fe73d64f513c1231314df1bcfa2974f7fc595
asc5025 <albert.s.chun@gmail.com>
2019-01-09 15:39:57 -0500

    seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.create(email: "albertchun@mail.com", password: "123", full_name: "the mouth" , img_url: "https://i.imgur.com/h7BvZ39.png")
-User.create(email: "mikeyu@mail.com", password: "123", full_name: "the mouth" , img_url: "https://i.imgur.com/9pIePlt.png")
-User.create(email: "caram@mail.com", password: "123", full_name: "VR" , img_url: "https://i.imgur.com/POp8O5e.png")
-User.create(email: "vera@mail.com", password: "123", full_name: "redux" , img_url: "https://i.imgur.com/0FnKUwG.png")
+User.create(email: "albertchun@mail.com", password: "123", full_name: "Albert Chun", img_url: "https://i.imgur.com/h7BvZ39.png", current_company: "Barbell Inc", current_industry: "Fitness", current_position: "Chief Savage Officer")
+User.create(email: "mikeyu@mail.com", password: "123", full_name: "Mike Yu", img_url: "https://i.imgur.com/9pIePlt.png", current_company: "Donations App", current_industry: "Non-Profit", current_position: "Founder")
+User.create(email: "caram@mail.com", password: "123", full_name: "Cara Morelli" , img_url: "https://i.imgur.com/POp8O5e.png", current_company: "VR World", current_industry: "Techonology", current_position: "Lead VR Developer")
+User.create(email: "vera@mail.com", password: "123", full_name: "Vera Protopopova" , img_url: "https://i.imgur.com/0FnKUwG.png", current_company: "BookFace", current_industry: "Social Media", current_position: "React Goddess")
