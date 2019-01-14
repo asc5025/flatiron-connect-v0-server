ef3768917a794bee9dc177fe9b5e157030357061
asc5025 <albert.s.chun@gmail.com>
2019-01-14 08:00:06 -0500

    update message controller and serializer

--- app/controllers/api/v1/messages_controller.rb
+++ app/controllers/api/v1/messages_controller.rb
@@ -10,7 +10,7 @@ class Api::V1::MessagesController < ApplicationController
 
   def create
     @message = @conversation.messages.new(message_params)
-    @message.user = current_user
+    # @message.user = current_user
     if @message.save
       render json: { message: MessageSerializer.new(@message) }, status: :created
     end
